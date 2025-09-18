ef56995beb5d3f6dbbf9b9e8cf3961fb95f8e2a8
kedharnadhG <g.kedharnadh098@gmail.com>
2025-09-18 12:16:12 +0530

    factors of a number

---
+++ MATH/FactorsOfN.java
@@ -0,0 +1,34 @@
+package MATH;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FactorsOfN {
+
+    public static void factors(int n, List<Integer> listOfFactors) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if(n/i == i) {
+                    listOfFactors.add(i);
+                } else {
+                    listOfFactors.add(i);
+                    listOfFactors.add(n/i);
+                }
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        int n = 12;
+
+        List<Integer> listOfFactors = new ArrayList<>();
+        factors(n, listOfFactors);
+
+        Collections.sort(listOfFactors);
+        System.out.println(listOfFactors);
+
+
+    }
+}
