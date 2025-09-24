a1f112d22ab5a516777bf8a100c24b3986c75b76
kedharnadhG <g.kedharnadh098@gmail.com>
2025-09-24 22:36:57 +0530

    prime factors added

--- MATH/FactorsOfN.java
+++ MATH/FactorsOfN.java
@@ -2,6 +2,7 @@ package MATH;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class FactorsOfN {
@@ -19,7 +20,42 @@ public class FactorsOfN {
         }
     }
     
-    // how to find the prime-factors is pending , 20/9 also pending  21/9 also pending  21/9 also pending   21/9 also pending
+    // how to find the prime-factors is pending ,
+            
+    /*
+        
+        Time Complexity: O(√n)
+        Space Complexity: O(log n)
+    
+     */            
+    public static HashMap<Long, Long> primeFactors(int n) {
+        //function to find the prime factors & their count
+
+        HashMap<Long, Long> factrCnt = new HashMap<>();
+
+        while (n % 2 == 0) {
+            factrCnt.put(2L, factrCnt.getOrDefault(2L, 0L) + 1);
+            n /= 2;
+        }
+
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+
+            //while i divides n, increment it's count
+
+            while (n % i == 0) {
+                factrCnt.put((long) i, factrCnt.getOrDefault((long) i, 0L) + 1);
+                n /= i;
+            }
+
+        }
+        
+        if (n > 2) {
+            factrCnt.put((long) n, factrCnt.getOrDefault((long) n, 0L) + 1);
+        }
+
+        return factrCnt;
+    }
     
 
     public static void main(String[] args) {
@@ -31,6 +67,13 @@ public class FactorsOfN {
         Collections.sort(listOfFactors);
         System.out.println(listOfFactors);
 
+
+        System.out.println("Prime factors of " + n + " are: ");
+        HashMap<Long, Long> factrCnt = primeFactors(n);
+
+        for (Long key : factrCnt.keySet()) {
+            System.out.println(key + " : " + factrCnt.get(key));
+        }
 
     }
 }
