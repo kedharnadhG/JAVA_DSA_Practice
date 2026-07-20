a7f6d732c4424f04098d8aea2f8729c3a851defe
kedharnadhG <g.kedharnadh098@gmail.com>
2026-07-20 16:57:27 +0530

    Count of No.Of Subarrays with sum k added

--- Hashing/CountOfSubArraySumK.java
+++ Hashing/CountOfSubArraySumK.java
@@ -164,3 +164,49 @@ public class CountOfSubArraySumK {
         // System.out.println("Number of Smallest subarrays with sum k is: " + ans[1]);
     }
 }
+
+
+
+
+/*Count of No.Of Subarrays with sum k
+Given an array of integers and an integer k, you need to find the count of subarrays with sum equal to k.
+
+            import java.util.HashMap;
+            import java.util.Map;
+            class GfG {
+                static int cntSubarrays(int[] arr, int k) {
+
+                    // HashMap to store prefix sums frequencies
+                    Map<Integer, Integer> prefixSums = new HashMap<>();
+                    int res = 0;
+                    int currSum = 0;
+
+                    for (int i = 0; i < arr.length; i++) {
+                        // Add current element to sum so far.
+                        currSum += arr[i];
+
+                        // If currSum is equal to desired sum
+                        // then a new subarray is found.
+                        if (currSum == k)
+                            res++;
+
+                        // Check if the difference exists in the prefixSums map.
+                        if (prefixSums.containsKey(currSum - k))
+                            res += prefixSums.get(currSum - k);
+
+                        // Add currSum to the set of prefix sums.
+                        prefixSums.put(currSum, prefixSums.getOrDefault(currSum, 0) + 1);
+                    }
+
+                    return res;
+                }
+
+                public static void main(String[] args) {
+                    int[] arr = {10, 2, -2, -20, 10};
+                    int k = -10;
+                    System.out.println(cntSubarrays(arr, k));
+                }
+            }
+
+
+*/
